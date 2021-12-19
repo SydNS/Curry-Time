d6f2b232f03474241f6a2a94b87706cd387a252e
SydNS <sydneynsiimbe@gmail.com>
2021-12-19 11:34:37 +0300

    added new views

--- app/src/main/java/com/example/currytime/screens/loginscreen.kt
+++ app/src/main/java/com/example/currytime/screens/loginscreen.kt
@@ -1,5 +1,6 @@
 package com.example.currytime.screens
 
+import android.graphics.drawable.PaintDrawable
 import android.text.method.TextKeyListener
 import android.widget.Toast
 import androidx.compose.foundation.Image
@@ -9,12 +10,17 @@ import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Home
+import androidx.compose.material.icons.filled.PlayArrow
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Alignment
+import androidx.compose.ui.Alignment.Companion.Center
 import androidx.compose.ui.Alignment.Companion.CenterHorizontally
+import androidx.compose.ui.Alignment.Companion.CenterVertically
+import androidx.compose.ui.Alignment.Companion.Top
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Brush
@@ -40,15 +46,14 @@ import com.example.currytime.ui.theme.dvgreentxt
 import com.google.android.material.textfield.TextInputEditText
 
 
-
-
-
-
 val imgcolor = Color(0xFF2B3141)
+
 //ui custom fonts
 val loginFont = FontFamily(Font(R.font.poppins))
 val boldloginFont = FontFamily(Font(R.font.poppinsbold))
 val semiloginFont = FontFamily(Font(R.font.poppinssemibold))
+
+var maxCharEmail: Int = 20
 
 @Preview
 @Composable
@@ -76,21 +81,21 @@ fun Loginscreen() {
                     painterResource(id = R.drawable.logo), contentDescription = "logo",
                     modifier = Modifier
                         .align(alignment = Alignment.CenterHorizontally)
-                        .padding(0.dp, 50.dp, 0.dp, 0.dp)
+                        .padding(0.dp, 20.dp, 0.dp, 0.dp)
                         .clip(RoundedCornerShape(10.dp))
-                        .height(190.dp)
-                        .width(190.dp)
+                        .height(150.dp)
+                        .width(150.dp)
                 )
                 Text(
 
                     color = dvgreentxt,
                     text = "Login",
                     fontWeight = FontWeight.ExtraBold,
-                    fontSize = 50.sp,
+                    fontSize = 30.sp,
                     textAlign = TextAlign.Center,
                     fontFamily = loginFont,
                     modifier = Modifier
-                        .padding(0.dp, 30.dp, 0.dp, 0.dp)
+                        .padding(0.dp, 10.dp, 0.dp, 0.dp)
                         .align(Alignment.CenterHorizontally)
 
 
@@ -103,15 +108,20 @@ fun Loginscreen() {
                 modifier = Modifier
                     .fillMaxSize()
                     .align(alignment = CenterHorizontally)
-                    .padding(start = 20.dp, end = 20.dp)
             ) {
                 MyTextField()
-                Spacer(modifier = Modifier.padding(0.dp, 10.dp))
+                Spacer(modifier = Modifier.padding(0.dp, 5.dp))
                 MyTextField()
-                Spacer(modifier = Modifier.padding(0.dp, 10.dp))
+                Spacer(modifier = Modifier.padding(0.dp, 5.dp))
                 forgotpass()
-                Spacer(modifier = Modifier.padding(0.dp, 10.dp))
+                Spacer(modifier = Modifier.padding(0.dp, 5.dp))
                 LoginButton()
+                Spacer(modifier = Modifier.padding(0.dp, 30.dp))
+                SocialAuthFacebook()
+                Spacer(modifier = Modifier.padding(0.dp, 5.dp))
+                SocialAuthGoogle()
+                Spacer(modifier = Modifier.padding(top = 25.dp))
+                NotaMember()
 
 
             }
@@ -119,13 +129,17 @@ fun Loginscreen() {
     }
 }
 
+
 @Preview
 @Composable
 fun MyTextField() {
     val textValue = remember { mutableStateOf("") }
     val primaryColor = colorResource(id = R.color.colorPrimary)
     OutlinedTextField(
+
         modifier = Modifier.padding(horizontal = 40.dp),
+        maxLines = 1,
+        singleLine = true,
         label = {
             Text(
                 text = stringResource(
@@ -146,9 +160,12 @@ fun MyTextField() {
         ),
         value = textValue.value,
         onValueChange = {
-            textValue.value = it
+            if (it.length <= maxCharEmail) {
+                textValue.value = it
+            }
         },
-    )
+
+        )
 }
 
 @Preview
@@ -161,7 +178,7 @@ fun forgotpass() {
         text = "Forgot your password ?",
         style = TextStyle(
 //            color = dvgreentxt,
-            fontFamily = loginFont,
+            fontFamily = semiloginFont,
             fontSize = 14.sp,
             fontWeight = FontWeight.Bold,
         )
@@ -176,13 +193,13 @@ fun LoginButton() {
         modifier = Modifier
             .fillMaxWidth()
             .padding(horizontal = 40.dp)
-            .background(color = dvgreenbtnbg)
+            .clip(shape = RoundedCornerShape(10.dp))
     ) {
         Text(
             text = "Login",
             color = Color.White,
             fontFamily = loginFont,
-            fontSize = 20.sp
+            fontSize = 25.sp
 
         )
     }
@@ -190,29 +207,124 @@ fun LoginButton() {
 
 @Preview
 @Composable
-fun SocialAuth(){
-    Column(modifier = Modifier
-        .fillMaxWidth()
-        .wrapContentHeight()){
-        Row(modifier = Modifier.fillMaxWidth()) {
+fun SocialAuthFacebook() {
+    Column(
+        modifier = Modifier
+            .fillMaxWidth()
+            .wrapContentWidth()
+            .wrapContentHeight()
+    ) {
+        Row(
+            modifier = Modifier
+                .width(260.dp)
+                .align(alignment = CenterHorizontally)
+        ) {
             Image(
-                painter = painterResource(id = com.example.currytime.R.drawable.facebook),
+                painter = painterResource(id = R.drawable.facebook),
                 "facebook",
-                Modifier.padding(start = 20.dp)
+                Modifier
+                    .padding(start = 20.dp, top = 5.dp, bottom = 5.dp)
+                    .wrapContentHeight()
+                    .size(20.dp), alignment = Center,
+                contentScale = ContentScale.Fit
             )
             Text(
                 modifier = Modifier
-                    .fillMaxWidth(),
-                text = "Forgot your password ?",
+                    .padding(5.dp, 5.dp),
+                text = "Sign In With Facebook",
                 style = TextStyle(
 //            color = dvgreentxt,
-                    fontFamily = loginFont,
-                    fontSize = 14.sp,
+                    fontFamily = semiloginFont,
+                    fontSize = 16.sp,
                     fontWeight = FontWeight.Bold,
+                    textAlign = TextAlign.Center
                 )
             )
-            
+
         }
 
     }
 }
+
+
+@Preview
+@Composable
+fun SocialAuthGoogle() {
+    Column(
+        modifier = Modifier
+            .fillMaxWidth()
+            .wrapContentWidth()
+            .wrapContentHeight()
+    )
+    {
+        Row(
+            modifier = Modifier
+                .width(260.dp)
+                .align(alignment = CenterHorizontally)
+        ) {
+            Image(
+                painter = painterResource(id = com.example.currytime.R.drawable.google),
+                "facebook",
+                Modifier
+                    .padding(start = 20.dp, top = 5.dp, bottom = 5.dp)
+                    .wrapContentHeight()
+                    .size(20.dp), alignment = Center,
+                contentScale = ContentScale.Fit
+
+            )
+            Text(
+                modifier = Modifier
+                    .padding(5.dp, 5.dp),
+                text = "Sign In With Google",
+                style = TextStyle(
+//            color = dvgreentxt,
+                    fontFamily = semiloginFont,
+                    fontSize = 16.sp,
+                    fontWeight = FontWeight.Bold,
+                    textAlign = TextAlign.Center
+                )
+            )
+
+        }
+
+    }
+}
+
+
+@Preview
+@Composable
+fun NotaMember() {
+
+    Row(
+        modifier = Modifier.fillMaxWidth(), horizontalArrangement = Arrangement.Center
+    ) {
+
+        Text(
+            modifier = Modifier
+                .padding(5.dp, 5.dp),
+            text = "Not A Member?",
+            style = TextStyle(
+//            color = dvgreentxt,
+                fontFamily = semiloginFont,
+                fontSize = 25.sp,
+                fontWeight = FontWeight.Bold,
+                textAlign = TextAlign.Center
+            )
+        )
+        Text(
+            modifier = Modifier
+                .padding(5.dp, 5.dp),
+            text = "Join Now",
+            style = TextStyle(
+                color = dvgreentxt,
+                fontFamily = semiloginFont,
+                fontSize = 25.sp,
+                fontWeight = FontWeight.Bold,
+                textAlign = TextAlign.Center
+            )
+        )
+
+    }
+
+}
+
