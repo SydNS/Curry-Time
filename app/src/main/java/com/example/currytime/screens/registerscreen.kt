de67e1ae2de4ae979d35f9fe8f1adea0c75a7286
SydNS <sydneynsiimbe@gmail.com>
2021-12-19 20:02:40 +0300

    resized and also added new fields

--- app/src/main/java/com/example/currytime/screens/registerscreen.kt
+++ app/src/main/java/com/example/currytime/screens/registerscreen.kt
@@ -1,29 +1,17 @@
 package com.example.currytime.screens
 
-import android.graphics.drawable.PaintDrawable
-import android.text.method.TextKeyListener
-import android.widget.Toast
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.*
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Home
-import androidx.compose.material.icons.filled.PlayArrow
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
-import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Alignment
-import androidx.compose.ui.Alignment.Companion.Center
-import androidx.compose.ui.Alignment.Companion.CenterHorizontally
-import androidx.compose.ui.Alignment.Companion.CenterVertically
-import androidx.compose.ui.Alignment.Companion.Top
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
-import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.colorResource
@@ -39,25 +27,23 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.currytime.R
-import com.example.currytime.ui.theme.bgcolor
-import com.example.currytime.ui.theme.bgcolor2
 import com.example.currytime.ui.theme.dvgreenbtnbg
 import com.example.currytime.ui.theme.dvgreentxt
-import com.google.android.material.textfield.TextInputEditText
+import com.example.currytime.ui.theme.dvgreentxtinput
+import com.example.currytime.ui.theme.empasscolor
 
-
-val imgcolor = Color(0xFF2B3141)
 
 //ui custom fonts
-val loginFont = FontFamily(Font(R.font.poppins))
-val boldloginFont = FontFamily(Font(R.font.poppinsbold))
-val semiloginFont = FontFamily(Font(R.font.poppinssemibold))
+val RegisterFont = FontFamily(Font(R.font.poppinsregular))
+val RegisterFontExtra = FontFamily(Font(R.font.poppinsextraligh))
+val boldRegisterFont = FontFamily(Font(R.font.poppinsbold))
+val semiRegisterFont = FontFamily(Font(R.font.poppinssemibold))
 
-var maxCharEmail: Int = 20
+var maxCharEmailRegister: Int = 20
 
 @Preview
 @Composable
-fun Loginscreen() {
+fun Registerscreen() {
     Box(
         modifier = Modifier
             .fillMaxSize()
@@ -66,7 +52,10 @@ fun Loginscreen() {
             )
     ) {
         Column(
-            modifier = Modifier.fillMaxSize()
+            modifier = Modifier.fillMaxWidth()
+                .wrapContentHeight()
+                .align(Alignment.Center)
+                .background(Color.Transparent)
         ) {
 
             Column(
@@ -74,54 +63,53 @@ fun Loginscreen() {
                     .wrapContentSize()
                     .background(color = Color.Transparent)
                     .align(alignment = Alignment.CenterHorizontally)
-                    .padding(start = 30.dp, top = 10.dp, end = 30.dp, bottom = 30.dp)
-
             ) {
                 Image(
                     painterResource(id = R.drawable.logo), contentDescription = "logo",
                     modifier = Modifier
                         .align(alignment = Alignment.CenterHorizontally)
-                        .padding(0.dp, 20.dp, 0.dp, 0.dp)
+                        .padding(0.dp, 0.dp, 0.dp, 0.dp)
                         .clip(RoundedCornerShape(10.dp))
                         .height(150.dp)
                         .width(150.dp)
                 )
                 Text(
-
                     color = dvgreentxt,
-                    text = "Login",
-                    fontWeight = FontWeight.ExtraBold,
+                    text = "Sign Up",
+                    fontWeight = FontWeight.Light,
                     fontSize = 30.sp,
                     textAlign = TextAlign.Center,
-                    fontFamily = loginFont,
+                    fontFamily = boldRegisterFont,
                     modifier = Modifier
-                        .padding(0.dp, 10.dp, 0.dp, 0.dp)
+                        .padding(0.dp, 0.dp, 0.dp, 0.dp)
                         .align(Alignment.CenterHorizontally)
 
 
                 )
             }
+
 //            var text by rememberSaveable { mutableStateOf("Text") }
 
 //            var text by remember { mutableStateOf("text") }
             Column(
                 modifier = Modifier
-                    .fillMaxSize()
-                    .align(alignment = CenterHorizontally)
+                    .wrapContentHeight()
+                    .wrapContentWidth()
+                    .padding(horizontal = 40.dp)
+                    .background(Color.Transparent)
+                    .align(alignment = Alignment.CenterHorizontally)
             ) {
-                MyTextField()
+                FirstNameTextFieldRegister()//firstname
                 Spacer(modifier = Modifier.padding(0.dp, 5.dp))
-                MyTextField()
+                LastNameTextFieldRegister   ()//lastname
                 Spacer(modifier = Modifier.padding(0.dp, 5.dp))
-                forgotpass()
+                EmailTextFieldRegister()//email address
                 Spacer(modifier = Modifier.padding(0.dp, 5.dp))
-                LoginButton()
-                Spacer(modifier = Modifier.padding(0.dp, 30.dp))
-                SocialAuthFacebook()
-                Spacer(modifier = Modifier.padding(0.dp, 5.dp))
-                SocialAuthGoogle()
-                Spacer(modifier = Modifier.padding(top = 25.dp))
-                NotaMember()
+                PasswordTextFieldRegister()//password
+                Spacer(modifier = Modifier.padding(0.dp, 20.dp))
+                RegisterButton()
+                Spacer(modifier = Modifier.padding( top=20.dp))
+                AlreadyaMember()
 
 
             }
@@ -129,30 +117,32 @@ fun Loginscreen() {
     }
 }
 
-
+//first name field
 @Preview
 @Composable
-fun MyTextField() {
+fun FirstNameTextFieldRegister() {
     val textValue = remember { mutableStateOf("") }
-    val primaryColor = colorResource(id = R.color.colorPrimary)
     OutlinedTextField(
-
-        modifier = Modifier.padding(horizontal = 40.dp),
+        modifier = Modifier.padding(horizontal = 0.dp)
+            .clip(shape = RoundedCornerShape(2.dp)),
         maxLines = 1,
+        isError = false,
         singleLine = true,
         label = {
             Text(
+                fontSize = 15.sp,
                 text = stringResource(
                     id =
-                    R.string.email
+                    R.string.firstname,
                 ),
-                fontWeight = FontWeight.SemiBold
+
+                fontWeight = FontWeight.SemiBold,
             )
         },
         colors = TextFieldDefaults.outlinedTextFieldColors(
-            focusedBorderColor = primaryColor,
-            focusedLabelColor = primaryColor,
-            cursorColor = primaryColor
+            focusedBorderColor = empasscolor,
+            focusedLabelColor = empasscolor,
+            cursorColor = empasscolor
         ),
         keyboardOptions = KeyboardOptions.Default.copy(
             keyboardType
@@ -160,7 +150,7 @@ fun MyTextField() {
         ),
         value = textValue.value,
         onValueChange = {
-            if (it.length <= maxCharEmail) {
+            if (it.length <= maxCharEmailRegister) {
                 textValue.value = it
             }
         },
@@ -168,46 +158,158 @@ fun MyTextField() {
         )
 }
 
+//last name field
 @Preview
 @Composable
-fun forgotpass() {
-    Text(
-        modifier = Modifier
-            .fillMaxWidth()
-            .padding(horizontal = 40.dp),
-        text = "Forgot your password ?",
-        style = TextStyle(
-//            color = dvgreentxt,
-            fontFamily = semiloginFont,
-            fontSize = 14.sp,
-            fontWeight = FontWeight.Bold,
+fun LastNameTextFieldRegister() {
+    val textValue = remember { mutableStateOf("") }
+    OutlinedTextField(
+        modifier = Modifier.padding(horizontal = 0.dp)
+            .clip(shape = RoundedCornerShape(2.dp)),
+        maxLines = 1,
+        isError = false,
+        singleLine = true,
+        label = {
+            Text(
+                fontSize = 15.sp,
+                text = stringResource(
+                    id =
+                    R.string.lastname,
+                ),
+
+                fontWeight = FontWeight.SemiBold,
+            )
+        },
+        colors = TextFieldDefaults.outlinedTextFieldColors(
+            focusedBorderColor = empasscolor,
+            focusedLabelColor = empasscolor,
+            cursorColor = empasscolor
+        ),
+        keyboardOptions = KeyboardOptions.Default.copy(
+            keyboardType
+            = KeyboardType.Email
+        ),
+        value = textValue.value,
+        onValueChange = {
+            if (it.length <= maxCharEmailRegister) {
+                textValue.value = it
+            }
+        },
+
         )
-    )
 }
 
+//emailfield
 @Preview
 @Composable
-fun LoginButton() {
+fun EmailTextFieldRegister() {
+    val textValue = remember { mutableStateOf("") }
+    OutlinedTextField(
+        modifier = Modifier.padding(horizontal = 0.dp)
+            .clip(shape = RoundedCornerShape(2.dp)),
+        maxLines = 1,
+        isError = false,
+        singleLine = true,
+        label = {
+            Text(
+                fontSize = 15.sp,
+                text = stringResource(
+                    id =
+                    R.string.email,
+                ),
+
+                fontWeight = FontWeight.SemiBold,
+            )
+        },
+        colors = TextFieldDefaults.outlinedTextFieldColors(
+            focusedBorderColor = empasscolor,
+            focusedLabelColor = empasscolor,
+            cursorColor = empasscolor
+        ),
+        keyboardOptions = KeyboardOptions.Default.copy(
+            keyboardType
+            = KeyboardType.Email
+        ),
+        value = textValue.value,
+        onValueChange = {
+            if (it.length <= maxCharEmailRegister) {
+                textValue.value = it
+            }
+        },
+
+        )
+}
+
+
+//password field
+@Preview
+@Composable
+fun PasswordTextFieldRegister() {
+    val textValue = remember { mutableStateOf("") }
+    OutlinedTextField(
+        modifier = Modifier.padding(horizontal = 0.dp)
+            .clip(shape = RoundedCornerShape(2.dp)),
+        maxLines = 1,
+        isError = false,
+        singleLine = true,
+        label = {
+            Text(
+                fontSize = 15.sp,
+                text = stringResource(
+                    id =
+                    R.string.password,
+                ),
+
+                fontWeight = FontWeight.SemiBold,
+            )
+        },
+        colors = TextFieldDefaults.outlinedTextFieldColors(
+            focusedBorderColor = empasscolor,
+            focusedLabelColor = empasscolor,
+            cursorColor = empasscolor
+        ),
+        keyboardOptions = KeyboardOptions.Default.copy(
+            keyboardType
+            = KeyboardType.Email
+        ),
+        value = textValue.value,
+        onValueChange = {
+            if (it.length <= maxCharEmailRegister) {
+                textValue.value = it
+            }
+        },
+
+        )
+}
+
+
+
+//@Preview
+@Composable
+fun RegisterButton() {
     Button(
         onClick = { },
+        colors = ButtonDefaults.buttonColors(
+            backgroundColor = dvgreenbtnbg,
+            contentColor = Color.White),
         modifier = Modifier
             .fillMaxWidth()
-            .padding(horizontal = 40.dp)
+            .padding(horizontal = 0.dp)
             .clip(shape = RoundedCornerShape(10.dp))
     ) {
         Text(
-            text = "Login",
+            text = "Sign Up",
             color = Color.White,
-            fontFamily = loginFont,
-            fontSize = 25.sp
+            fontFamily = RegisterFontExtra,
+            fontSize = 18.sp
 
         )
     }
 }
 
-@Preview
+//@Preview
 @Composable
-fun SocialAuthFacebook() {
+fun SocialAuthFacebookRegister() {
     Column(
         modifier = Modifier
             .fillMaxWidth()
@@ -217,7 +319,7 @@ fun SocialAuthFacebook() {
         Row(
             modifier = Modifier
                 .width(260.dp)
-                .align(alignment = CenterHorizontally)
+                .align(alignment = Alignment.CenterHorizontally)
         ) {
             Image(
                 painter = painterResource(id = R.drawable.facebook),
@@ -225,7 +327,7 @@ fun SocialAuthFacebook() {
                 Modifier
                     .padding(start = 20.dp, top = 5.dp, bottom = 5.dp)
                     .wrapContentHeight()
-                    .size(20.dp), alignment = Center,
+                    .size(20.dp), alignment = Alignment.Center,
                 contentScale = ContentScale.Fit
             )
             Text(
@@ -234,7 +336,7 @@ fun SocialAuthFacebook() {
                 text = "Sign In With Facebook",
                 style = TextStyle(
 //            color = dvgreentxt,
-                    fontFamily = semiloginFont,
+                    fontFamily = semiRegisterFont,
                     fontSize = 16.sp,
                     fontWeight = FontWeight.Bold,
                     textAlign = TextAlign.Center
@@ -247,9 +349,9 @@ fun SocialAuthFacebook() {
 }
 
 
-@Preview
+//@Preview
 @Composable
-fun SocialAuthGoogle() {
+fun SocialAuthGoogleRegister() {
     Column(
         modifier = Modifier
             .fillMaxWidth()
@@ -260,15 +362,15 @@ fun SocialAuthGoogle() {
         Row(
             modifier = Modifier
                 .width(260.dp)
-                .align(alignment = CenterHorizontally)
+                .align(alignment = Alignment.CenterHorizontally)
         ) {
             Image(
-                painter = painterResource(id = com.example.currytime.R.drawable.google),
+                painter = painterResource(id = R.drawable.google),
                 "facebook",
                 Modifier
                     .padding(start = 20.dp, top = 5.dp, bottom = 5.dp)
                     .wrapContentHeight()
-                    .size(20.dp), alignment = Center,
+                    .size(20.dp), alignment = Alignment.Center,
                 contentScale = ContentScale.Fit
 
             )
@@ -278,7 +380,7 @@ fun SocialAuthGoogle() {
                 text = "Sign In With Google",
                 style = TextStyle(
 //            color = dvgreentxt,
-                    fontFamily = semiloginFont,
+                    fontFamily = semiRegisterFont,
                     fontSize = 16.sp,
                     fontWeight = FontWeight.Bold,
                     textAlign = TextAlign.Center
@@ -291,9 +393,9 @@ fun SocialAuthGoogle() {
 }
 
 
-@Preview
+//@Preview
 @Composable
-fun NotaMember() {
+fun AlreadyaMember() {
 
     Row(
         modifier = Modifier.fillMaxWidth(), horizontalArrangement = Arrangement.Center
@@ -302,11 +404,11 @@ fun NotaMember() {
         Text(
             modifier = Modifier
                 .padding(5.dp, 5.dp),
-            text = "Not A Member?",
+            text = "Already A Member?",
             style = TextStyle(
 //            color = dvgreentxt,
-                fontFamily = semiloginFont,
-                fontSize = 25.sp,
+                fontFamily = semiRegisterFont,
+                fontSize = 20.sp,
                 fontWeight = FontWeight.Bold,
                 textAlign = TextAlign.Center
             )
@@ -314,11 +416,11 @@ fun NotaMember() {
         Text(
             modifier = Modifier
                 .padding(5.dp, 5.dp),
-            text = "Join Now",
+            text = "Login",
             style = TextStyle(
                 color = dvgreentxt,
-                fontFamily = semiloginFont,
-                fontSize = 25.sp,
+                fontFamily = semiRegisterFont,
+                fontSize = 20.sp,
                 fontWeight = FontWeight.Bold,
                 textAlign = TextAlign.Center
             )
