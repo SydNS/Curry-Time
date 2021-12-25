4ef4eb793c8d3cade4d3f4ffbbd596a7d7dbebfd
SydNS <sydneynsiimbe@gmail.com>
2021-12-25 22:30:48 +0300

    new screens

--- app/src/main/java/com/example/currytime/screens/CurryTimeNavigation.kt
+++ app/src/main/java/com/example/currytime/screens/CurryTimeNavigation.kt
@@ -1,13 +1,17 @@
 package com.example.currytime.screens
 
 import androidx.compose.animation.ExperimentalAnimationApi
+import androidx.compose.material.ExperimentalMaterialApi
 import androidx.compose.runtime.Composable
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import com.example.currytime.screens.dashboardscreens.*
+import com.example.currytime.screens.dashboardscreens.HomeScreen
 import com.google.accompanist.pager.ExperimentalPagerApi
 
 
+@ExperimentalMaterialApi
 @ExperimentalPagerApi
 @ExperimentalAnimationApi
 @Composable
@@ -20,7 +24,13 @@ fun Navigation() {
         composable("splash") { Splash(navController) }
         composable("Login") { Loginscreen(navController) }
         composable("Register") { Registerscreen(navController) }
-
+//        dashboard
+        composable("Dashboard") { DashboardView(navController) }
+//        dashboar screens
+        composable("home") { Home(navController) }
+        composable("profile") { Profile(navController) }
+        composable("search") { Search(navController) }
+        composable("notifications") { Notifications(navController) }
 
     }
 
