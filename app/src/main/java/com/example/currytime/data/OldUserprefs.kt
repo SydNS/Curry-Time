07e58a0cde7b9dd3788be6b418ff1b895d04b6e2
SydNS <sydneynsiimbe@gmail.com>
2021-12-30 16:20:39 +0300

    this is the file that will be used to check-in new users into old ones

--- app/src/main/java/com/example/currytime/data/OldUserprefs.kt
+++ app/src/main/java/com/example/currytime/data/OldUserprefs.kt
@@ -1,6 +1,41 @@
 package com.example.currytime.data
 
-//SharedPreferences sharedPref = getSharedPreferences("name", Context.MODE_PRIVATE);
-//SharedPreferences.Editor editor = sharedPref.edit();
-//editor.putString("key", "Value");
-//editor.commit();
+import android.content.Context
+import android.content.SharedPreferences
+import androidx.datastore.core.DataStore
+import androidx.datastore.preferences.core.Preferences
+import androidx.datastore.preferences.core.edit
+import androidx.datastore.preferences.core.emptyPreferences
+import androidx.datastore.preferences.core.preferencesKey
+import androidx.datastore.preferences.createDataStore
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.catch
+import kotlinx.coroutines.flow.map
+import java.io.IOException
+
+
+class DataStoreManager(val context: Context) {
+
+    private val dataStore: DataStore<Preferences> = context.createDataStore(name = "useroldornew")
+
+    private object PreferenceKeys {
+        val olduser = preferencesKey<String>("user_status")
+    }
+
+
+    suspend fun saveToDatastore(old: String) {
+        dataStore.edit {
+            it[PreferenceKeys.olduser] = old
+        }
+
+    }
+
+    suspend fun getFromDataStore() = dataStore.data.map {
+        val mystatus = it[PreferenceKeys.olduser] ?: "none"
+        mystatus
+
+    }
+}
+
+
+
